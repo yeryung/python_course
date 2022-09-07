d070167d5a5e78def05f154c938e9f27b966af26
ery270 <ery270@naver.com>
2022-09-07 09:04:53 +0900

    commit day2

---
+++ myfile.py
@@ -0,0 +1,13 @@
+from threenames import a,b,c
+title = 'The meaning of life'
+print(a)
+print(b)
+print(c)
+
+x = a,b,c
+print(x)
+
+exec('print(2**100)')
+exec('c= 10+20\nprint(c)')
+
+print("\u2764")
